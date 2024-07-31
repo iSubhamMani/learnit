2057fba8ff18aeecc3af34fa57e2254d9afaa5a4
Subham Mani <manisubham09@gmail.com>
2024-07-31 18:40:13 +0530

    Add Tag and TagInput components

---
+++ src/components/Tag.tsx
@@ -0,0 +1,33 @@
+import { MouseEventHandler } from "react";
+
+const Tag = ({
+  value,
+  onClickHandler,
+}: {
+  value: string;
+  onClickHandler: MouseEventHandler;
+}) => {
+  return (
+    <div
+      onClick={onClickHandler}
+      className="badge badge-secondary gap-2 p-3 cursor-pointer"
+    >
+      <span className="text-secondary-content">{value}</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="inline-block h-4 w-4 stroke-secondary-content"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6 18L18 6M6 6l12 12"
+        ></path>
+      </svg>
+    </div>
+  );
+};
+
+export default Tag;
