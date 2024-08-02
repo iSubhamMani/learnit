aa85b9233fd33962b3df3b70bdf75a83ff6977c2
Subham Mani <manisubham09@gmail.com>
2024-08-02 20:53:57 +0530

    add submission logic and error handling

--- src/app/u/ask/page.tsx
+++ src/app/u/ask/page.tsx
@@ -3,8 +3,15 @@
 import TagInput from "@/components/TagInput";
 import useTagInput from "@/hooks/useTagInput";
 import { discussionSchema, tagSchema } from "@/schemas/DiscussionSchema";
+import { SERVER_ERROR_MESSAGE } from "@/utils/constants";
 import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { LoaderCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
 
 const AskPage = () => {
   const {
@@ -29,15 +36,54 @@ const AskPage = () => {
     tags,
   } = useTagInput();
 
-  const onSubmit = (formData: any) => {
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+
+  const onSubmit = (formData: z.infer<typeof discussionSchema>) => {
     const { success, data, error } = tagSchema.safeParse(tags);
     if (error) {
       setTagError(error.errors[0].message);
     }
     if (!success) return;
-    console.log(formData);
-    console.log(data);
+
+    submitDiscussion({ ...formData, tags: data });
   };
+
+  async function submitDiscussion(
+    discussionData: z.infer<typeof discussionSchema> & { tags: string[] }
+  ) {
+    setSubmitting(true);
+
+    try {
+      const response = await axios.post("/api/discussion", discussionData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("learnit-token")}`,
+        },
+      });
+
+      if (response.data.success) {
+        toast.success("Discussion posted successfully", {
+          duration: 3000,
+          position: "top-center",
+        });
+        router.replace("/u/discussions");
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.message, {
+          duration: 3000,
+          position: "top-center",
+        });
+      } else {
+        toast.error(SERVER_ERROR_MESSAGE, {
+          duration: 3000,
+          position: "top-center",
+        });
+      }
+    } finally {
+      setSubmitting(false);
+    }
+  }
 
   return (
     <div className="px-5 md:pl-24 py-8 w-full flex flex-col bg-base-200">
@@ -93,7 +139,11 @@ const AskPage = () => {
               </div>
 
               <button type="submit" className="btn btn-primary">
-                Submit
+                {submitting ? (
+                  <LoaderCircle className="animate-spin" />
+                ) : (
+                  "Submit"
+                )}
               </button>
             </form>
           </div>
