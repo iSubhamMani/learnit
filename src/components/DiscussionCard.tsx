2b2fdf3824d921a8f57e8ec84ed8753ec3604b74
Subham Mani <manisubham09@gmail.com>
2024-08-03 12:20:32 +0530

    Update DiscussionCard component to display discussion details

--- src/components/DiscussionCard.tsx
+++ src/components/DiscussionCard.tsx
@@ -1,43 +1,65 @@
 "use client";
 
+import { DiscussionData } from "@/interfaces/discussion.interface";
+import { convertDateTime } from "@/utils/convertDateTime";
 import { CalendarIcon } from "lucide-react";
 import Image from "next/image";
 
-const DiscussionCard = () => {
+const DiscussionCard = ({ discussion }: { discussion: DiscussionData }) => {
   return (
     <div className="w-full max-w-xl card bg-base-100 shadow-md rounded-md">
       <div className="card-body p-4 sm:p-6 md:p-8">
         <div className="card-title">
           <p className="text-lg sm:text-xl text-base-content font-bold">
-            How do I set up a CI/CD pipeline for my React app?
+            {discussion.title}
           </p>
         </div>
-        <div className="flex flex-wrap gap-3 my-1">
+        <div className="flex flex-wrap gap-3 my-2">
           <div className="flex items-center gap-2">
-            <div className="avatar">
-              <div className="w-4 rounded-full">
-                <Image src="" alt="avatar" width={16} height={16} />
+            {discussion.askedBy.photoURL ? (
+              <div className="avatar">
+                <div className="ring-neutral-content ring-offset-base-100 ring-1 ring-offset-2 w-4 sm:w-6 rounded-full">
+                  <Image
+                    src={discussion.askedBy.photoURL}
+                    alt="avatar"
+                    width={24}
+                    height={24}
+                  />
+                </div>
               </div>
-            </div>
+            ) : (
+              <div className="avatar placeholder">
+                <div className="ring-neutral-content ring-offset-base-100 ring-1 ring-offset-2 bg-neutral text-neutral-content w-4 sm:w-6 rounded-full">
+                  <span className="text-xs">
+                    {discussion.askedBy.displayName.charAt(0).toUpperCase()}
+                  </span>
+                </div>
+              </div>
+            )}
             <p className="text-xs sm:text-sm text-base-content font-medium">
-              John Doe
+              {discussion.askedBy.displayName}
             </p>
           </div>
           <div className="flex items-center gap-2">
             <CalendarIcon className="w-4 h-4 text-base-content/85" />
             <p className="text-xs sm:text-sm text-base-content/85">
-              2 days ago
+              {convertDateTime(discussion.createdAt)}
             </p>
           </div>
         </div>
         <div className="flex flex-wrap gap-2">
-          <div className="badge badge-secondary">react</div>
+          {discussion.tags.map((tag, index) => (
+            <div
+              key={index}
+              className="badge badge-secondary text-xs sm:text-sm font-normal"
+            >
+              {tag}
+            </div>
+          ))}
         </div>
         <div className="mt-4">
           <p className="text-sm sm:text-base text-base-content line-clamp-2">
-            Im building a React app and I want to set up a CI/CD pipeline to
-            automate my builds and deployments. Can someone guide me through the
-            process?
+            {discussion.description}
           </p>
         </div>
       </div>
