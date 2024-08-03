fa949db4e57b672e68802350dcd2e68cfec32a95
Subham Mani <manisubham09@gmail.com>
2024-08-03 12:20:46 +0530

    add discussion data type

--- src/interfaces/discussion.interface.ts
+++ src/interfaces/discussion.interface.ts
@@ -4,3 +4,17 @@ export interface Discussion {
   tags: string[];
   askedBy: string;
 }
+
+export type DiscussionData = Pick<
+  Discussion,
+  "title" | "description" | "tags"
+> & {
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  askedBy: {
+    _id: string;
+    displayName: string;
+    photoURL: string;
+  };
+};
