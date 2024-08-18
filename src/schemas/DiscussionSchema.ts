84e2a9855a736e23c5d61c0a9cf38c7af550e49e
Subham Mani <manisubham09@gmail.com>
2024-08-18 13:37:45 +0530

    Add validation for discussion attachment size and type

--- src/schemas/DiscussionSchema.ts
+++ src/schemas/DiscussionSchema.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
+const MAX_FILE_SIZE = 4000000;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
 
 export const discussionSchema = z.object({
   title: z
@@ -8,6 +10,34 @@ export const discussionSchema = z.object({
   description: z
     .string()
     .min(10, { message: "Description should be at least 10 characters" }),
+  attachment:
+    typeof window === "undefined"
+      ? z.any().nullable()
+      : z
+          .instanceof(FileList)
+          .nullable()
+          .refine(
+            (files) => {
+              const file = files?.item(0);
+              if (!file) return true;
+
+              return file?.size <= MAX_FILE_SIZE;
+            },
+            {
+              message: `Image size should be less than ${
+                MAX_FILE_SIZE / 1000000
+              }MB.`,
+            }
+          )
+          .refine(
+            (files) => {
+              const file = files?.item(0);
+              if (!file) return true;
+
+              return ACCEPTED_IMAGE_TYPES.includes(file?.type);
+            },
+            { message: "Only JPEG, JPG, and PNG images are allowed." }
+          ),
 });
 
 export const tagSchema = z
