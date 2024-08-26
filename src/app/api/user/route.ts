f70bdb73f7ff42e14ef677371ec2e84e484adeb4
Subham Mani <manisubham09@gmail.com>
2024-08-26 13:24:13 +0530

    Update user signup route to include password hashing and photo upload

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,45 +4,84 @@ import { newUser } from "@/types/UserRequestBody";
 import { ApiError } from "@/utils/ApiError";
 import { ApiSuccess } from "@/utils/ApiSuccess";
 import { NextRequest, NextResponse } from "next/server";
-import { SignJWT } from "jose";
-import secretKey from "@/utils/encodeJWT";
+import bcrypt from "bcryptjs";
+import { uploadToCloudinary } from "@/lib/cloudinary/uploadToCloudinary";
+import { signupSchema } from "@/schemas/signup.schema";
 
 export async function POST(req: NextRequest) {
   await connectDB();
 
   try {
-    const { _id, displayName, email, photoURL } = (await req.json()) as newUser;
+    const formData = await req.formData();
 
-    if (!_id || !displayName || !email || !photoURL) {
-      return NextResponse.json(new ApiError(400, "Missing required fields"), {
+    const displayName = formData.get("displayName") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const photo = formData.get("photo") as File | null;
+
+    const { success } = signupSchema.safeParse({
+      displayName,
+      email,
+      password,
+      photo,
+    });
+
+    if (!success) {
+      return NextResponse.json(new ApiError(400, "Invalid request body"), {
         status: 400,
       });
     }
 
-    const existingUser = await UserModel.findOne({ _id });
+    const existingUser = await UserModel.findOne({ email });
 
     if (existingUser) {
-      const token = await new SignJWT({ _id: existingUser._id })
-        .setProtectedHeader({ alg: "HS256" })
-        .sign(secretKey);
-
       return NextResponse.json(
-        new ApiSuccess(200, "User already exists", {
-          user: existingUser,
-          token,
-        }),
+        new ApiSuccess(400, "User already exists", null),
         {
-          status: 200,
+          status: 400,
         }
       );
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    let photoURL = "";
+
+    if (photo) {
+      const fileBuffer = await photo.arrayBuffer();
+
+      const mimeType = photo.type;
+      const encoding = "base64";
+      const base64Data = Buffer.from(fileBuffer).toString("base64");
+
+      // this will be used to upload the file
+      const fileUri = "data:" + mimeType + ";" + encoding + "," + base64Data;
+
+      const res = await uploadToCloudinary(
+        fileUri,
+        photo.name,
+        "profile-photos"
+      );
+
+      if (!res) {
+        return NextResponse.json(
+          new ApiError(500, "Failed to upload attachment"),
+          {
+            status: 500,
+          }
+        );
+      }
+
+      photoURL = res.secure_url;
+    }
+
     // Save the user to the database
     const createdUser = await UserModel.create({
-      _id,
       displayName,
       email,
       photoURL,
+      password: hashedPassword,
     });
 
     if (!createdUser) {
@@ -51,19 +90,12 @@ export async function POST(req: NextRequest) {
       });
     }
 
-    const token = await new SignJWT({ _id: createdUser._id })
-      .setProtectedHeader({ alg: "HS256" })
-      .sign(secretKey);
-
     return NextResponse.json(
-      new ApiSuccess(201, "User created successfully", {
-        user: createdUser,
-        token,
-      }),
+      new ApiSuccess(201, "User created successfully", createdUser),
       { status: 201 }
     );
-  } catch (error) {
-    return NextResponse.json(new ApiError(500, "Internal Server Error"), {
+  } catch (error: any) {
+    return NextResponse.json(new ApiError(500, error.message), {
       status: 500,
     });
   }
