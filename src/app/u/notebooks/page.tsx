1c08d914ba92a78de40e1edb507c0feccc296144
Subham Mani <manisubham09@gmail.com>
2024-08-26 13:24:43 +0530

    Replace create notebook button with NewNotebookModal component

--- src/app/u/notebooks/page.tsx
+++ src/app/u/notebooks/page.tsx
@@ -1,14 +1,11 @@
-import { FilePlus } from "lucide-react";
+import NewNotebookModal from "@/components/NewNotebookModal";
 
 const NotebooksPage = () => {
   return (
     <div className="px-6 md:pl-24 py-8 w-full flex flex-col bg-base-200">
       <h1 className="text-2xl text-base-content">Your Notebooks</h1>
       <div className="mt-4">
-        <button className="btn btn-sm sm:btn-md btn-primary">
-          <FilePlus className="w-4 h-4 sm:w-5 sm:h-5" />
-          <span className="text-sm">Create</span>
-        </button>
+        <NewNotebookModal />
       </div>
     </div>
   );
