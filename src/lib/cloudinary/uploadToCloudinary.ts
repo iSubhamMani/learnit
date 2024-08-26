db613a1d0bbe01a45bd9251707869957d25119b5
Subham Mani <manisubham09@gmail.com>
2024-08-26 13:20:18 +0530

    Add folderName parameter to uploadToCloudinary function

--- src/lib/cloudinary/uploadToCloudinary.ts
+++ src/lib/cloudinary/uploadToCloudinary.ts
@@ -1,11 +1,15 @@
 import cloudinary from "@/lib/cloudinary/config";
 
-export const uploadToCloudinary = async (fileUri: string, fileName: string) => {
+export const uploadToCloudinary = async (
+  fileUri: string,
+  fileName: string,
+  folderName: string
+) => {
   const res = await cloudinary.uploader.upload(fileUri, {
     invalidate: true,
     resource_type: "image",
     filename_override: fileName,
-    folder: "learnit",
+    folder: "learnit" + folderName,
     use_filename: true,
   });
 
