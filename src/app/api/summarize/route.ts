ed8f97d241c800253e86e2ab9d78ca55c77e016b
Subham Mani <manisubham09@gmail.com>
2024-09-09 20:53:37 +0530

    Add POST route for generating summaries from images

---
+++ src/app/api/summarize/route.ts
@@ -0,0 +1,76 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
+import { writeFile } from "fs/promises";
+import path from "path";
+import { ApiSuccess } from "@/utils/ApiSuccess";
+import { ApiError } from "@/utils/ApiError";
+import { SummaryModel } from "@/models/summary.model";
+
+function fileToGenerativePart(path: any, mimeType: any) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType,
+    },
+  };
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const formData = await req.formData();
+    const userId = req.headers.get("user-id");
+    const notebookId = formData.get("notebookId") as string;
+
+    const genAI = new GoogleGenerativeAI(
+      process.env.NEXT_PUBLIC_GOOGLE_AI_API_KEY!
+    );
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const mediaPath = formData.get("mediaPath") as File;
+    const buffer = Buffer.from(await mediaPath.arrayBuffer());
+    const filename = Date.now() + mediaPath.name.replaceAll(" ", "_");
+
+    const filePath = path.join(process.cwd(), "public/uploads/" + filename);
+
+    await writeFile(filePath, buffer);
+
+    const prompt = `
+    Please analyze the provided image and generate a concise summary of its educational content. Focus on the core ideas and avoid unnecessary details. The output should be formatted as given in the example, the topic and points separated by a # and each points seperated by a \n character.
+  
+    **Example:**
+  
+    The Solar System
+    #
+      The Sun is at the center\n
+      There are eight planets\n
+      The planets are divided into two groups: terrestrial and gas giants.
+  
+    **End of Example:**
+  
+    If the image does not contain any educational content return null
+    `;
+
+    const imagePart = fileToGenerativePart(`${filePath}`, mediaPath.type);
+
+    const result = await model.generateContent([prompt, imagePart]);
+    fs.unlinkSync(filePath);
+
+    if (result.response.text() !== "null") {
+      // TODO: Save summary to database
+      await SummaryModel.create({
+        content: result.response.text(),
+        generatedBy: userId,
+        notebook: notebookId,
+      });
+    }
+
+    return NextResponse.json(
+      new ApiSuccess(200, "Success", result.response.text())
+    );
+  } catch (error: any) {
+    return NextResponse.json(new ApiError(500, error.message), {
+      status: 500,
+    });
+  }
+}
