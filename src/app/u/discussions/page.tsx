55f4ff3515335888c8214479b174c3e95becd062
Subham Mani <manisubham09@gmail.com>
2024-09-10 19:34:51 +0530

    Update discussions page UI and functionality

--- src/app/u/discussions/page.tsx
+++ src/app/u/discussions/page.tsx
@@ -2,9 +2,10 @@
 
 import DiscussionCard from "@/components/DiscussionCard";
 import DiscussionCardSkeleton from "@/components/DiscussionCardSkeleton";
+import StyledButton from "@/components/StyledButton";
 import { DiscussionCardData } from "@/interfaces/discussion.interface";
 import { getAllDiscussions } from "@/queries/discussion.queries";
-import { Frown, MoveRight } from "lucide-react";
+import { Frown, MoveRight, Search } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 import { useInfiniteQuery, useQueryClient } from "react-query";
@@ -13,7 +14,6 @@ import { useDebounce } from "use-debounce";
 const Discussions = () => {
   const [filter, setFilter] = useState<string>("");
   const [debouncedFilter] = useDebounce(filter, 350);
-  const queryClient = useQueryClient();
 
   const {
     fetchNextPage,
@@ -40,7 +40,7 @@ const Discussions = () => {
     <div className="px-5 md:pl-24 py-8 w-full flex flex-col bg-base-200">
       <h1 className="text-2xl text-base-content">Discussion Board</h1>
       <div className="mt-4">
-        <label className="input input-bordered flex items-center gap-2 max-w-xs">
+        <label className="input input-bordered flex items-center gap-2 max-w-sm">
           <input
             value={filter}
             onChange={(e) => setFilter(e.target.value)}
@@ -48,27 +48,11 @@ const Discussions = () => {
             className="grow text-base-content"
             placeholder="Search by title or tags"
           />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70"
-          >
-            <path
-              fillRule="evenodd"
-              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <Search className="w-5 h-5 text-base-content" />
         </label>
         <div className="mt-6 flex justify-start">
           <Link href="/u/ask">
-            <div className="px-4 py-2 w-max flex gap-2 items-center rounded-full bg-primary bg-opacity-10 hover:bg-opacity-20 transition ease-in-out duration-200">
-              <p className="text-primary text-base font-medium">
-                Start a discussion
-              </p>
-              <MoveRight className="w-5 h-5 text-primary" />
-            </div>
+            <StyledButton content="Start a discussion" />
           </Link>
         </div>
       </div>
