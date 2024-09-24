e23dbaad9f5b9ba55b6c160896caf4206d75f817
Subham Mani <manisubham09@gmail.com>
2024-09-24 15:11:40 +0530

    Update button styles

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -9,7 +9,7 @@ const BackButton = () => {
   return (
     <button
       onClick={() => router.back()}
-      className="cursor-pointer bg-white hover:bg-white/55 border dark:border-none dark:bg-neutral dark:hover:bg-neutral/75 rounded-full p-2"
+      className="cursor-pointer bg-white hover:bg-white/55 border dark:border-none dark:bg-neutral dark:hover:bg-neutral/75 rounded-full p-2 transform active:scale-95 transition duration-150"
     >
       <ArrowLeft className="w-4 h-4 sm:w-5 sm:h-5 text-base-content" />
     </button>
--- src/components/LoadMoreButton.tsx
+++ src/components/LoadMoreButton.tsx
@@ -5,7 +5,7 @@ const LoadMoreButton = ({ clickHandler }: { clickHandler: () => void }) => {
   return (
     <button
       onClick={() => clickHandler()}
-      className="text-sm text-primary font-bold px-4 py-2 w-max flex gap-2 items-center rounded-full bg-primary bg-opacity-10 hover:bg-opacity-20 transition ease-in-out duration-200"
+      className="text-sm text-primary font-bold px-4 py-2 w-max flex gap-2 items-center rounded-full bg-primary bg-opacity-10 hover:bg-opacity-20 transition ease-in-out transform active:scale-95 duration-150"
     >
       Load more
       <RotateCcw className="w-4 h-4 text-primary" />
