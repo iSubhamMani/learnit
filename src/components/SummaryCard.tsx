3742a3a12d0bc6f3829f1ffd40ebcb28c58e21f8
Subham Mani <manisubham09@gmail.com>
2024-10-22 19:54:05 +0530

    Refactor QuizCard and SummaryCard components to use separate delete modals

--- src/components/SummaryCard.tsx
+++ src/components/SummaryCard.tsx
@@ -2,6 +2,7 @@ import { Summary } from "@/interfaces/summary.interface";
 import { parseData } from "@/utils/parseSummaryData";
 import { ArrowLeft } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import DeleteSummaryModel from "./DeleteSummaryModal";
 
 const SummaryCard = ({ summary }: { summary: Summary & { _id: string } }) => {
   const [topic, setTopic] = useState("");
@@ -15,50 +16,55 @@ const SummaryCard = ({ summary }: { summary: Summary & { _id: string } }) => {
   }, [summary]);
 
   return (
-    <div
-      onClick={() =>
-        (
-          document.getElementById(`${summary._id}`)! as HTMLDialogElement
-        ).showModal()
-      }
-      className="cursor-pointer card shadow-md dark:shadow-2xl bg-white/50 dark:bg-neutral/55 text-primary-content max-w-md rounded-md"
-    >
-      <div className="card-body">
-        <h2 className="text-base-content card-title font-bold line-clamp-2">
-          {topic}
-        </h2>
-        <p className="text-base-content/85 line-clamp-2">{points}</p>
+    <div>
+      <div className="flex mb-2 justify-end">
+        <DeleteSummaryModel summary={summary} />
       </div>
-      <dialog id={`${summary._id}`} className="modal">
-        <div className="modal-box max-w-4xl cursor-default">
-          <form className="my-2" method="dialog">
-            <button>
-              <ArrowLeft className="w-6 h-6 text-primary" />
-            </button>
-          </form>
-          <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 text-base-content">
-            {topic && topic}
-          </h1>
-          {points.length > 0 && (
-            <h2 className="text-lg md:text-xl font-semibold mb-2 text-base-content">
-              Key Points:
-            </h2>
-          )}
-          <ul className="list-disc list-inside space-y-2">
-            {points.map((point, index) => (
-              <li
-                key={index}
-                className="text-base md:text-lg text-base-content"
-              >
-                {point}
-              </li>
-            ))}
-          </ul>
+      <div
+        onClick={() =>
+          (
+            document.getElementById(`${summary._id}`)! as HTMLDialogElement
+          ).showModal()
+        }
+        className="cursor-pointer card shadow-md dark:shadow-2xl bg-white/50 dark:bg-neutral/55 text-primary-content max-w-md rounded-md"
+      >
+        <div className="card-body">
+          <h2 className="text-base-content card-title font-bold line-clamp-2">
+            {topic}
+          </h2>
+          <p className="text-base-content/85 line-clamp-2">{points}</p>
         </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
+        <dialog id={`${summary._id}`} className="modal">
+          <div className="modal-box max-w-4xl cursor-default">
+            <form className="my-2" method="dialog">
+              <button>
+                <ArrowLeft className="w-6 h-6 text-primary" />
+              </button>
+            </form>
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4 text-base-content">
+              {topic && topic}
+            </h1>
+            {points.length > 0 && (
+              <h2 className="text-lg md:text-xl font-semibold mb-2 text-base-content">
+                Key Points:
+              </h2>
+            )}
+            <ul className="list-disc list-inside space-y-2">
+              {points.map((point, index) => (
+                <li
+                  key={index}
+                  className="text-base md:text-lg text-base-content"
+                >
+                  {point}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+      </div>
     </div>
   );
 };
