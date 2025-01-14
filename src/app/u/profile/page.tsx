31707c4616b90b82711f6b9f01c2e749475bdbcf
Subham Mani <manisubham09@gmail.com>
2025-01-14 11:36:29 +0530

    update

--- src/app/u/profile/page.tsx
+++ src/app/u/profile/page.tsx
@@ -84,14 +84,6 @@ const ProfilePage = () => {
           )}
         </div>
       </div>
-      <div className="flex justify-center mt-4">
-        <Link href="/u/profile/edit">
-          <div className="px-4 py-2 w-max flex gap-2 items-center rounded-full bg-primary bg-opacity-10 hover:bg-opacity-20 transform active:scale-95 transition duration-150">
-            <p className="text-primary text-base font-medium">Edit Profile</p>
-            <EditIcon className="w-5 h-5 text-primary" />
-          </div>
-        </Link>
-      </div>
       <div className="my-6">
         <h1 className="text-base-content text-xl md:text-2xl font-medium">
           Latest Discussions
